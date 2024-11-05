db6b7a09e21ac92e4769de01e9b5b979a73068f5
wannaDevelopIt <cdaosldk@naver.com>
2024-11-06 07:16:21 +0900

    10강 강의 내용

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 //@EnableWebSecurity
 @Configuration
@@ -12,6 +19,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("custom entry point");
+            }
+        }).and().build(); // custom 설정한 경우 기본 form login 또는 basic 로그인 entrypoint 가 exceptionTransactionFilter 에 제공되지 않는다
+
         return http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().apply(new CustomSecurityConfigurer().setFlag(false)).and().build();
     }
 }
